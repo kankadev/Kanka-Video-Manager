e3279ce275ddc481c1ed38ca6ef6d48bcac3572b
Akay <mail@kanka.dev>
2022-06-03 00:15:22 +0300

    Skeleton

--- src/main/java/dev/kanka/kankavideomanager/ui/controller/MainController.java
+++ src/main/java/dev/kanka/kankavideomanager/ui/controller/MainController.java
@@ -3,12 +3,10 @@ package dev.kanka.kankavideomanager.ui.controller;
 import dev.kanka.kankavideomanager.enums.MEDIA_STATUS;
 import dev.kanka.kankavideomanager.pojo.KnkMedia;
 import dev.kanka.kankavideomanager.ui.common.FxController;
-import dev.kanka.kankavideomanager.utils.MediaStatusUtil;
-import javafx.beans.binding.Bindings;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableArray;
-import javafx.collections.ObservableList;
 import javafx.collections.ObservableSet;
+import javafx.css.PseudoClass;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
@@ -21,7 +19,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.kordamp.ikonli.javafx.FontIcon;
 import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
-import uk.co.caprica.vlcj.media.callback.CallbackMedia;
+import uk.co.caprica.vlcj.javafx.view.ResizableImageView;
+import uk.co.caprica.vlcj.media.MediaRef;
 import uk.co.caprica.vlcj.player.base.MediaPlayer;
 import uk.co.caprica.vlcj.player.base.MediaPlayerEventAdapter;
 import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
@@ -32,11 +31,18 @@ import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import static org.kordamp.ikonli.fontawesome5.FontAwesomeRegular.PAUSE_CIRCLE;
+import static org.kordamp.ikonli.fontawesome5.FontAwesomeRegular.PLAY_CIRCLE;
+import static org.kordamp.ikonli.materialdesign2.MaterialDesignD.DELETE;
+import static org.kordamp.ikonli.materialdesign2.MaterialDesignF.FILE_MOVE;
+import static org.kordamp.ikonli.materialdesign2.MaterialDesignP.PLAYLIST_CHECK;
+import static org.kordamp.ikonli.materialdesign2.MaterialDesignP.PLAYLIST_REMOVE;
+import static org.kordamp.ikonli.materialdesign2.MaterialDesignS.*;
 import static uk.co.caprica.vlcj.javafx.videosurface.ImageViewVideoSurfaceFactory.videoSurfaceForImageView;
 
 public class MainController implements FxController {
 
-    private static final Logger logger = LogManager.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger();
 
     private static MainController instance;
 
@@ -47,8 +53,20 @@ public class MainController implements FxController {
     private List<Button> controlButtons;
     private List<Button> playListButtons;
 
+    private static KnkMedia currentPlayingMedia;
+    
+    private static PseudoClass PLAYING_PSEUDO_CLASS = PseudoClass.getPseudoClass("playing");
+    private static PseudoClass FINISHED_PSEUDO_CLASS = PseudoClass.getPseudoClass("finished");
+
+
     @FXML
     BorderPane borderPane;
+
+    @FXML
+    Slider timeSlider, volumeSlider;
+
+    @FXML
+    Button playPauseBtn, stopBtn, previousBtn, nextBtn, skipBackwardBtn, skipForwardBtn, deleteBtn, moveBtn;
 
     @FXML
     TableView<KnkMedia> playList;
@@ -63,7 +81,7 @@ public class MainController implements FxController {
     TableColumn<KnkMedia, String> fileSizeColumn;
 
     @FXML
-    Button playPauseBtn, stopBtn, previousBtn, nextBtn, skipBackwardBtn, skipForwardBtn, deleteBtn, moveBtn;
+    Label countFilesLabel, volumeLabel;
 
     @FXML
     Button emptyPlaylistBtn, processAllFilesBtn;
@@ -81,6 +99,7 @@ public class MainController implements FxController {
         playListButtons = FXCollections.observableArrayList(emptyPlaylistBtn, processAllFilesBtn);
 
         initPlayer();
+        initTimeSlider();
         initControlButtons();
         initPlayList();
         initPlayListButtons();
@@ -93,49 +112,83 @@ public class MainController implements FxController {
 
         this.embeddedMediaPlayer.events().addMediaPlayerEventListener(new MediaPlayerEventAdapter() {
             @Override
+            public void mediaChanged(MediaPlayer mediaPlayer, MediaRef media) {
+                LOGGER.debug("mediaPlayer: {}, media: {}", mediaPlayer, media);
+                timeSlider.setMin(0);
+                timeSlider.setValue(0);
+            }
+
+            @Override
             public void playing(MediaPlayer mediaPlayer) {
-                stopBtn.setDisable(false);
-                playPauseBtn.setText("Pause");
+                LOGGER.debug("mediaPlayer: {}", mediaPlayer);
+                timeSlider.setMax(mediaPlayer.media().info().duration());
+
+                Platform.runLater(() -> {
+                    playPauseBtn.setText("Pause");
+                    playPauseBtn.setGraphic(new FontIcon(PAUSE_CIRCLE));
+                });
             }
 
             @Override
             public void paused(MediaPlayer mediaPlayer) {
-                stopBtn.setDisable(false);
-                playPauseBtn.setText("Play");
+                LOGGER.debug("mediaPlayer: {}", mediaPlayer);
+                Platform.runLater(() -> {
+                    playPauseBtn.setText("Play");
+                    playPauseBtn.setGraphic(new FontIcon(PLAY_CIRCLE));
+                });
             }
 
             @Override
             public void stopped(MediaPlayer mediaPlayer) {
-                stopBtn.setDisable(true);
-                playPauseBtn.setText("Play");
+                LOGGER.debug("mediaPlayer: {}", mediaPlayer);
+            }
+
+            @Override
+            public void finished(MediaPlayer mediaPlayer) {
+                LOGGER.debug("mediaPlayer: {}", mediaPlayer);
             }
 
             @Override
             public void timeChanged(MediaPlayer mediaPlayer, long newTime) {
                 // TODO implement timeline
+                LOGGER.debug("newTime: {}", newTime);
+                timeSlider.setValue(newTime);
+            }
+
+            @Override
+            public void positionChanged(MediaPlayer mediaPlayer, float newPosition) {
+                //LOGGER.debug("newPosition: {}", newPosition);
+//                timeSlider.setValue(newPosition * 100);
+            }
+
+            @Override
+            public void volumeChanged(MediaPlayer mediaPlayer, float volume) {
+                // TODO update volume label
+                LOGGER.debug("mediaPlayer: {}, volume: {}", mediaPlayer, volume);
             }
         });
 
-        this.videoImageView = new ImageView();
-        this.videoImageView.setPreserveRatio(true);
-
-        videoImageView.fitWidthProperty().bind(borderPane.widthProperty());
-        videoImageView.fitHeightProperty().bind(borderPane.heightProperty());
-
         borderPane.widthProperty().addListener((observableValue, oldValue, newValue) -> {
             // If you need to know about resizes
+            LOGGER.debug("borderPane's width: {}", newValue);
         });
 
         borderPane.heightProperty().addListener((observableValue, oldValue, newValue) -> {
             // If you need to know about resizes
+            LOGGER.debug("borderPane's height: {}", newValue);
         });
 
         this.videoImageView = new ImageView();
+        ResizableImageView resizableImageView = new ResizableImageView(videoImageView);
         this.videoImageView.setPreserveRatio(true);
-
         this.embeddedMediaPlayer.videoSurface().set(videoSurfaceForImageView(this.videoImageView));
 
-        borderPane.setCenter(videoImageView);
+        borderPane.setCenter(resizableImageView);
+        borderPane.getCenter().getStyleClass().setAll("videoFrame");
+    }
+
+    private void initTimeSlider() {
+
     }
 
     private void initControlButtons() {
@@ -143,8 +196,39 @@ public class MainController implements FxController {
             btn.disableProperty().bind(playList.getSelectionModel().selectedItemProperty().isNull());
         }
 
-        playPauseBtn.setOnAction(event -> play());
-        stopBtn.setOnAction(event -> stop());
+        Platform.runLater(() -> {
+            playPauseBtn.setText("Play");
+            playPauseBtn.setGraphic(new FontIcon(PLAY_CIRCLE));
+            playPauseBtn.setOnAction(event -> play());
+
+            stopBtn.setOnAction(event -> stop());
+            stopBtn.setGraphic(new FontIcon(STOP_CIRCLE));
+
+            previousBtn.setGraphic(new FontIcon(SKIP_PREVIOUS));
+            nextBtn.setGraphic(new FontIcon(SKIP_NEXT));
+
+            skipBackwardBtn.setGraphic(new FontIcon(SKIP_BACKWARD));
+            skipBackwardBtn.setOnAction(event -> {
+                if (embeddedMediaPlayer != null && embeddedMediaPlayer.media().info() != null) {
+                    embeddedMediaPlayer.controls().skipTime(embeddedMediaPlayer.media().info().duration() / 20 * -1);
+                }
+            });
+            skipForwardBtn.setGraphic(new FontIcon(SKIP_FORWARD));
+            skipForwardBtn.setOnAction(event -> {
+                if (embeddedMediaPlayer != null && embeddedMediaPlayer.media().info() != null) {
+                    embeddedMediaPlayer.controls().skipTime((embeddedMediaPlayer.media().info().duration() / 20));
+                }
+            });
+
+            deleteBtn.setGraphic(new FontIcon(DELETE));
+            moveBtn.setGraphic(new FontIcon(FILE_MOVE));
+
+            emptyPlaylistBtn.setGraphic(new FontIcon(PLAYLIST_REMOVE));
+            processAllFilesBtn.setGraphic(new FontIcon(PLAYLIST_CHECK));
+
+            embeddedMediaPlayer.audio().setVolume((int) volumeSlider.getValue() / 100);
+            volumeSlider.valueProperty().addListener((observable, oldValue, newValue) -> embeddedMediaPlayer.audio().setVolume(newValue.intValue()));
+        });
     }
 
     private void initPlayList() {
@@ -179,6 +263,9 @@ public class MainController implements FxController {
 
         // File Size Column
         fileSizeColumn.setCellValueFactory(new PropertyValueFactory<>("fileSize"));
+
+        // Count files in playList // TODO: not working on "empty playlist"
+        playList.itemsProperty().addListener(observable -> countFilesLabel.setText(playList.getItems().size() + " files in playlist"));
     }
 
     private void initPlayListButtons() {
@@ -219,7 +306,7 @@ public class MainController implements FxController {
 
                 success = true;
 
-                logger.debug("Added file(s) to playlist: {}", newFiles);
+                LOGGER.debug("Added file(s) to playlist: {}", newFiles);
             }
 
             /*
@@ -236,23 +323,36 @@ public class MainController implements FxController {
     }
 
     private void play() {
-        if(embeddedMediaPlayer != null ) {
+        if (embeddedMediaPlayer != null) {
 //            embeddedMediaPlayer.controls().play();
             embeddedMediaPlayer.media().play(playList.getItems().get(0).getAbsolutePath());
         }
     }
 
-    public void stop() {
+    private void stop() {
         if (embeddedMediaPlayer != null) {
             embeddedMediaPlayer.controls().stop();
+        }
+    }
+
+    private void release() {
+        if (embeddedMediaPlayer != null) {
             embeddedMediaPlayer.release();
+        }
+        if (mediaPlayerFactory != null) {
             mediaPlayerFactory.release();
         }
+    }
+
+    public void stopAndRelease() {
+        stop();
+        release();
     }
 
     private void emptyPlaylist() {
         stop();
         playList.getItems().clear();
+        countFilesLabel.setText("0 files in playlist");
     }
 
 }
